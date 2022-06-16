d43122b42edc631ad6431bab95d82197c833c983
Davi Lhlapak Rosa <davilhlapak@hotmail.com>
2022-06-16 17:23:35 -0300

    feat: added font configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -5,6 +7,9 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        fontFamily: {
+            sans: ["Ubuntu", ...defaultTheme.fontFamily.sans]
+        },
         extend: {},
     },
     plugins: [],
