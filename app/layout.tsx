bc6ababa57c4849882c79e4ac6b4703d1f70ec6c
Davi Lhlapak Rosa <davilhlapak@hotmail.com>
2022-11-16 17:12:42 -0300

    refactor(next): updated some pages into new nextjs app dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,11 @@ export default function RootLayout({
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="robots" content="index,follow" />
             </head>
-            <body className="min-h-screen h-full font-sans">{children}</body>
+            <body className="min-h-screen h-full w-full font-sans">
+                <main className="w-full h-full flex-grow flex flex-col items-center [&>div]:w-full">
+                    {children}
+                </main>
+            </body>
         </html>
     )
 }
